93c8176196dea079092a07223739e750200a247d
Viven-Naidoo <naidooviven16@gmail.com>
2024-08-31 09:24:22 +0200

    Added API

--- src/app.js
+++ src/app.js
@@ -1,4 +1,14 @@
-function searchCity(city) {}
+function refreshWeather(response) {
+  console.log(response.data);
+}
+
+function searchCity(city) {
+  let apiKey = "95t503a47d4232o4806ad4bb3df835e4";
+  let apiUrl =
+    "https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}";
+  axios.get(apiUrl).then(refreshWeather);
+  console.log(apiUrl);
+}
 
 function handleSearch(event) {
   event.preventDefault();
