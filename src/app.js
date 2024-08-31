bb260e6da0e0b5b5e05e493a8b8734b462fa6ae1
Viven-Naidoo <naidooviven16@gmail.com>
2024-08-31 08:20:00 +0200

    Updated JS

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
+function searchCity(city) {}
+
 function handleSearch(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
